491efa2b3759d489e331172aa531e91328d2f578
jabelone <hello@jaimyn.com.au>
2017-10-25 15:56:22 +1000

    update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='avocentpdu',
-    version='1.0',
+    version='1.0.0',
     packages=[''],
     url='https://github.com/jabelone/avocentpdu',
     license='GNU GPL V3 or later',
