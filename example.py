69a17ec302e9fa10da81d4018036139b69afe1d7
Jaimyn Mayer <hello@jaimyn.com.au>
2017-11-02 03:13:41 +1000

    fixed typo in example

--- example.py
+++ example.py
@@ -7,6 +7,6 @@ import pdu
 # The instance will then save that into a variable so any
 # further requests will not have to login again.
 #pdu = avocentpdu.PDU("USERNAME", "PASSWORD", "https://IP")
-pdu = avocentpdu.PDU("admin", "1234", "https://192.168.0.7")
+pdu = pdu.PDU("admin", "1234", "https://192.168.0.7")
 pdu.switch_outlet(3, 0) # switch the third outlet off
 pdu.switch_outlet(5, 1) # switch the fifth outlet on
